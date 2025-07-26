3278d6669456dc72c7ba7ca1c91281b7bb9063d5
AnbhuleSanchit123 <anbhulesanchit@gmail.com>
2025-07-26 18:36:58 +0530

    Adding java programs

---
+++ Arithmetic.java
@@ -0,0 +1,12 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        int A=10;
+        int B=5;
+        System.out.println("Addition is:"+ (A+B));
+        System.out.println("Substraction is:"+ (A-B));
+        System.out.println("Multipliction is:"+ (A*B));
+        System.out.println("Division is:" + (A/B));
+        System.out.println("Reminder is:" + (A%B));
+    }
+    
+}
---
+++ AssigmetOperator.java
@@ -0,0 +1,12 @@
+public class AssigmetOperator {
+    public static void main (String args[]){
+    int a=5;
+    int b=a;
+    System.out.println(b);
+    b-=3;
+    System.out.println(b);
+    a*=3;
+    System.out.println(a);
+    
+    }
+}
